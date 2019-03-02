99563eb7b2bbcf0206a8627f96063ee317cd967b
Jazmin Rojas <jazred996@gmail.com>
2019-03-02 19:52:23 +0000

    Advice for the day

---
+++ script.js
@@ -0,0 +1,16 @@
+let request = new XMLHttpRequest();
+let url = "https://api.adviceslip.com/advice";
+
+request.open("GET", url, true);
+
+request.onload = function(){
+  //Begin accessing JSON data here. The data stored in request.respons
+  let data = JSON.parse(this.response);
+  let advice = document.getElementById('advice');
+  
+  if (request.status >= 200 && request.status < 400) {
+     advice.innerHTML = data["slip"].advice;
+  }
+};
+
+request.send();
